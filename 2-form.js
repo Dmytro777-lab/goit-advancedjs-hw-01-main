66db390f7d9cca087191e6145df61be7172a423b
Dmytro777-lab <dorofeyev74@gmail.com>
2025-05-29 12:20:09 +0000

    Deploying to gh-pages from @ Dmytro777-lab/goit-advancedjs-hw-01-main@6af0f323952cd1e361a7164acf22ea5a65213ef4 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";const t=document.querySelector(".feedback-form"),m="feedback-form-state";let e=JSON.parse(localStorage.getItem(m))||{};e.email&&(t.elements.email.value=e.email);e.message&&(t.elements.message.value=e.message);t.addEventListener("input",a=>{e[a.target.name]=a.target.value.trim(),localStorage.setItem(m,JSON.stringify(e))});t.addEventListener("submit",a=>{a.preventDefault(),console.log("Sending data:",e),localStorage.removeItem(m),t.reset(),e={}});
+//# sourceMappingURL=2-form.js.map
